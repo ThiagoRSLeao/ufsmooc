f20b86c8978b19634919dd9d5b969ee5060603a6
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2020-12-12 19:22:01 -0300

    Update ControllerUser.php

--- app/Http/Controllers/ControllerUser.php
+++ app/Http/Controllers/ControllerUser.php
@@ -7,23 +7,39 @@ use Illuminate\Support\Facades\Auth;
 
 class ControllerUser extends Controller
 {
+
     public function userLogin()
     {
         return view('pages.login');
     }
+
+
     public function validateLogin(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, 0)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('dashboard');
+            return redirect()->intended('/login');
         }
-
+ 
         return back()->withErrors([
             'email' => 'As credenciais inseridas não estão registradas no sistema.',
         ]);
+    }
+
+    public function endSession(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+    
+        $request->session()->regenerateToken();
+    
+        return redirect('/');
+    }
+
+    public function registerUser(){
+
     }
 }
 
